dbdbe22a632523391c0105a777526cadb098a524
asphodel6 <harmsss@mail.ru>
2023-03-17 16:38:57 +0500

    Сверстал header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,11 @@ import style from './Header.module.css'
 
 function Header() {
     return (
-        <div className={style.header}>Header</div>
+        <div className={style.header}>
+            <div className={style.logo}><img src="images/logo.svg" alt="logo" /></div>
+            <div className={style.loginButton}><button>Вход</button></div>
+            <div className={style.registerButton}><button>Регистрация</button></div>
+        </div>
     );
 }
 
